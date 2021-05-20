969b6a38367ebb0d908cd27dd77fdc4603b2565d
Kevin Suhajda <kevin@nevercode.io>
2021-05-20 12:09:38 +0200

    add scripts folder

---
+++ flutter/flutter-flavors-demo-project/scripts/build_dev.sh
@@ -0,0 +1,20 @@
+# Buid DEV version of app
+# Codemagic API documentation can be found here - https://docs.codemagic.io/rest-api/builds/
+# Update appId, workflowId, branch, APP_STORE_ID, BUNDLE_ID, XCODE_SCHEME, XCODE_CONFIG, ENTRY_POINT to your own values.
+
+curl -H "Content-Type: application/json" -H "x-auth-token: ${CM_API_KEY}" \
+--data '{
+    "appId": "5faaaca7e55b87f29c8f246b", 
+    "workflowId": "ios-release",
+    "branch": "flavors", 
+    "environment": { 
+        "variables": { 
+            "APP_STORE_ID": "1565355012",
+            "BUNDLE_ID": "io.nevercode.flutterapp.dev",
+            "XCODE_SCHEME": "dev", 
+            "XCODE_CONFIG": "Release-dev",
+            "ENTRY_POINT": "lib/main_dev.dart"
+        }
+    }
+}' \
+https://api.codemagic.io/builds
