05ec26c9c097ebd3070cbcfdf4d20c933daf74a9
Kevin Suhajda <kevin@nevercode.io>
2021-06-11 13:12:10 +0200

    update app version

--- react-native/react-native-demo-project/android/app/src/main/java/com/cmreactnative/MainApplication.java
+++ react-native/react-native-demo-project/android/app/src/main/java/com/cmreactnative/MainApplication.java
@@ -1,4 +1,4 @@
-package com.codemagicreactnative;
+package com.cmreactnative;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.codemagicreactnative.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.cmreactnative.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- react-native/react-native-demo-project/ios/cmreactnative/AppDelegate.m
+++ react-native/react-native-demo-project/ios/cmreactnative/AppDelegate.m
@@ -1,4 +1,3 @@
-#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -28,19 +27,20 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  }
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"codemagicreactnative"
+                                                   moduleName:@"cmreactnative"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+      rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
