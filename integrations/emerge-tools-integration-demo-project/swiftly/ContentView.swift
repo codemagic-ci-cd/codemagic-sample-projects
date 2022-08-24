ff20f607d34644a4c867d687e5621543813b31f8
nevercode-rudrank <96109109+nevercode-rudrank@users.noreply.github.com>
2022-08-24 17:03:22 +0530

    Add Emerge integration (#115)
    * Add Emerge integration
    
    * Update integrations/emerge-tools-integration-demo-project/codemagic.yaml
    
    Co-authored-by: nevercode-kevin <68225534+nevercode-kevin@users.noreply.github.com>
    
    Co-authored-by: Rudrank Riyam <rudrankriyam@gmail.com>
    Co-authored-by: nevercode-kevin <68225534+nevercode-kevin@users.noreply.github.com>

---
+++ integrations/emerge-tools-integration-demo-project/swiftly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftly
+//
+//  Created by Rudrank Riyam on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ integrations/emerge-tools-integration-demo-project/swiftly/swiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlyApp.swift
+//  swiftly
+//
+//  Created by Rudrank Riyam on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
