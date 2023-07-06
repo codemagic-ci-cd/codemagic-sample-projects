9d62270abf6f0353ce5a9299c51020bac2b03f30
Himesh Panchal <106069365+himesh-cm@users.noreply.github.com>
2023-07-06 14:17:32 +0530

    csi-setup

---
+++ ios/ios-code-signing-identities-demo-project/swiftly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftly
+//
+//  Created by Kevin Freeman on 12/08/2020.
+//  Copyright © 2020 Kevin Freeman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
