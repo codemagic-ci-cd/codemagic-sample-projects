3415c3539944002c1d796e7e50b993a5d1b8d22e
dtrdic <david@nevercode.io>
2023-08-30 13:17:01 +0200

    added maui net7 unsigned app example

---
+++ dotnet-maui/dotnet7-maui-unsigned-ios-app/MauiNet7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiNet7_2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ dotnet-maui/dotnet7-maui-unsigned-ios-app/MauiNet7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiNet7_2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
