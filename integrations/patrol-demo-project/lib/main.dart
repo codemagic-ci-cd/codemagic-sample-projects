e78f330047853d3c3be9b875bcc9d00fe33b495e
Kwiatek Szymon <ken1998@wp.pl>
2025-06-06 14:43:44 +0200

    Make example simple

--- integrations/patrol-demo-project/lib/main.dart
+++ integrations/patrol-demo-project/lib/main.dart
@@ -1,96 +1,69 @@
-import 'package:animations/animations.dart';
-import 'package:example/cubit/auth_cubit.dart';
-import 'package:example/firebase_options.dart';
-import 'package:example/handlers/notification_handler.dart';
-import 'package:example/pages/home_page.dart';
-import 'package:example/pages/push_notification/notification_success_page.dart';
-import 'package:example/ui/style/colors.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:provider/provider.dart';
 
-const firebaseEnabled = String.fromEnvironment('FIREBASE_ENABLED') == 'true';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initApp();
-  debugPrint('!Google sign in will not work when dev flavor used!');
-
+void main() {
   runApp(const MyApp());
 }
-
-Future<void> initApp() async {
-  _setUpTheme();
-
-  if (firebaseEnabled) {
-    await _initFirebase();
-    await _askForLocationPermission();
-  }
-}
-
-void _setUpTheme() {
-  final mySystemTheme = SystemUiOverlayStyle.light
-      .copyWith(systemNavigationBarColor: PTColors.lcBlack);
-
-  SystemChrome.setSystemUIOverlayStyle(mySystemTheme);
-}
-
-Future<void> _initFirebase() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
-
-Future<void> _askForLocationPermission() => Geolocator.requestPermission();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<AuthCubit>(
-      create: (context) => AuthCubit(
-        firebaseEnabled ? FirebaseAuth.instance : null,
-        firebaseEnabled ? GoogleSignIn() : null,
-      )..init(),
-      child: Provider(
-        lazy: false,
-        create: (_) => NotificationHandler(
-          FlutterLocalNotificationsPlugin(),
-          firebaseEnabled ? FirebaseMessaging.instance : null,
-        )..init(() => Navigator.push(context, notificationRoute)),
-        child: MaterialApp(
-          theme: ThemeData.dark().copyWith(
-            pageTransitionsTheme: const PageTransitionsTheme(
-              builders: {
-                TargetPlatform.android: SharedAxisPageTransitionsBuilder(
-                  transitionType: SharedAxisTransitionType.horizontal,
-                ),
-                TargetPlatform.iOS: SharedAxisPageTransitionsBuilder(
-                  transitionType: SharedAxisTransitionType.horizontal,
-                ),
-              },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.grey),
-            primaryColor: PTColors.lcBlack,
-            canvasColor: PTColors.textDark,
-            textSelectionTheme: TextSelectionThemeData(
-              selectionColor: PTColors.lcYellow.withValues(alpha: 0.5),
-              cursorColor: PTColors.textWhite,
-              selectionHandleColor: PTColors.lcYellow,
+            Text(
+              key: const Key('counter_text'),
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-          debugShowCheckedModeBanner: false,
-          title: 'Patrol Challenge',
-          home: const HomePage(),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('increment_button'),
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
--- integrations/patrol-demo-project/lib/ui/icons.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_svg/flutter_svg.dart';
-
-abstract class PTIcons {
-  static final appIcon = SvgPicture.asset('assets/icon/app_icon.png');
-  static final circleX = SvgPicture.asset('assets/icon/circle_x.svg');
-}
