75f152e94cdee1339b04efa22b5b51b6989a801f
david <dtrdic2@gmail.com>
2026-05-19 14:17:45 +0200

    initial

--- react-native/react-native-detox-demo-project/metro.config.js
+++ react-native/react-native-detox-demo-project/metro.config.js
@@ -1,11 +1,13 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
  *
- * @format
+ * @type {import('metro-config').MetroConfig}
  */
 
-module.exports = {
+const config = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -15,3 +17,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
